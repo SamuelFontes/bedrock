af6c47902d839e0c15d9c8f2cd2859f9dbd1b9d5
SamuelFontes <samuguel@hotmail.com.br>
2024-07-11 04:56:03 -0300

    Testing if I can identify if compile script is running into WSL

--- compile.sh
+++ compile.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-cl65 main.asm --verbose --target nes -o /mnt/c/tools/fceux/game/bedrock.nes
+# Check if it's WSL or regular distro
+WSLFILE=/proc/sys/fs/binfmt_misc/WSLInterop
+if [ -f "$WSLFILE" ]; then
+	echo "$WSLFILE exists. It's Windows"
+else
+	echo "$WSLFILE does not exist. It's Linux"
+fi
+#cl65 main.asm --verbose --target nes -o /mnt/c/tools/fceux/game/bedrock.nes
+#cl65 main.asm --verbose --target nes -o build/bedrock.nes
