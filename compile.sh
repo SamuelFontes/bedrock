1744372b6c81df1240f6483aa9a8f0157a1bf904
SamuelFontes <samuguel@hotmail.com.br>
2024-07-11 05:00:10 -0300

    Added multiple compilation paths to handle WSL

--- compile.sh
+++ compile.sh
@@ -2,9 +2,11 @@
 # Check if it's WSL or regular distro
 WSLFILE=/proc/sys/fs/binfmt_misc/WSLInterop
 if [ -f "$WSLFILE" ]; then
-	echo "$WSLFILE exists. It's Windows"
+	echo "File '$WSLFILEFile' exists. Compilling to WSL."
+	cl65 main.asm --verbose --target nes -o /mnt/c/tools/fceux/game/bedrock.nes
+	echo "Rom compiled to c/tools/fceux/game/bedrock.nes"
 else
-	echo "$WSLFILE does not exist. It's Linux"
+	echo "File '$WSLFILE' does not exist so it's not WSL. Compilling it to 'build' folder."
+	cl65 main.asm --verbose --target nes -o build/bedrock.nes
+	echo "Rom compiled to build/bedrock.nes folder."
 fi
-#cl65 main.asm --verbose --target nes -o /mnt/c/tools/fceux/game/bedrock.nes
-#cl65 main.asm --verbose --target nes -o build/bedrock.nes
